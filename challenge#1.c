bbad1765e0e870c1abd8dfe254db7ca59d4dba88
Jhon Mark Casiracan <markycasiracan@gmail.com>
2026-07-04 17:21:55 +0800

    Update challenge#1.c

--- challenge#1.c
+++ challenge#1.c
@@ -73,3 +73,41 @@ int main(void) {
                 printf("Invalid choice. Please select 1-6.\n");
         }
         printf("\n");
+  } while (choice != 6);
+    return 0;
+}
+void clearInputBuffer(void) {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        
+    }
+}
+void addStudent(void) {
+    if (studentCount >= MAX_STUDENTS) {
+        printf("Registry is full! Cannot add more than %d students.\n", MAX_STUDENTS);
+    }
+
+    int idx = studentCount; 
+
+    printf("\n--- Add New Student ---\n");
+    printf("Enter Student ID: ");
+    scanf("%d", &ids[idx]);
+    clearInputBuffer();
+
+    for (int i = 0; i < studentCount; i++) {
+        if (ids[i] == ids[idx]) {
+            printf("A student with ID %d already exists. Student not added.\n", ids[idx]);
+        }
+    }
+    
+    printf("Enter Full Name: ");
+    fgets(names[idx], NAME_LEN, stdin);
+    names[idx][strcspn(names[idx], "\n")] = '\0'; 
+
+    printf("Enter Major: ");
+    fgets(majors[idx], MAJOR_LEN, stdin);
+    majors[idx][strcspn(majors[idx], "\n")] = '\0';
+
+    printf("Enter GPA (0.0 - 4.0): ");
+    scanf("%f", &gpas[idx]);
+    clearInputBuffer();
